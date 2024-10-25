c7f85ab037362467aee6b650b2815942668d4e06
agray <agray@qa.com>
2024-10-25 13:00:40 +0100

    CREATE configure-mysql-debian.sh

---
+++ configure-mysql-debian.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo -i
+echo "Please enter the password for the mysql ADMIN user"
+read pword
+echo "Please enter mysql ROOT password (note: you will need to re-enter this password when prompted during mysql installation)"
+read rootPword
+
+apt-get update
+apt-get install extrepo
+sed -i -e 's/# -/-/g;' /etc/extrepo/config.yaml
+extrepo update
+extrepo enable mysql
+apt-get update
+apt-get install mysql-server
+
+echo "bind-address = 0.0.0.0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+mysql -u root --password=$rootPword <<EOF
+CREATE USER 'admin'@'localhost' IDENTIFIED BY '$pword';
+CREATE USER 'admin'@'%' IDENTIFIED BY '$pword';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+EOF
+
+systemctl restart mysql.service
